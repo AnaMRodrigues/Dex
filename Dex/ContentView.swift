4795e61ea24b3bd8003f9f03331878d37c22e8dd
Ana <aninha_s2.acrm@icloud.com>
2025-07-23 10:34:26 -0300

    Add search bar

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -11,12 +11,30 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
-        animation: .default)
-    private var pokedex: FetchedResults<Pokemon>
+    @FetchRequest<Pokemon>(
+        sortDescriptors: [SortDescriptor(\.id)],
+        animation: .default
+    ) private var pokedex
+    
+    @State private var searchText = ""
     
     let fetcher = FetchService()
+    
+    private var dynamicPredicate: NSPredicate {
+        var predicates: [NSPredicate] = []
+        
+        //Search Predicate
+        if !searchText.isEmpty {
+            //[c] is for the code don't worry about the case sensitivity(Uppercase, lowercase)
+            predicates.append(NSPredicate(format: "name contains[c] %@", searchText))
+        }
+        
+        //filter by favorite predicate
+        
+        
+        //combine predicates
+        return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+    }
 
     var body: some View {
         NavigationStack {
@@ -53,6 +71,11 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Pokedex")
+            .searchable(text: $searchText, prompt: "Find a Pokemon")
+            .autocorrectionDisabled()
+            .onChange(of: searchText) {
+                pokedex.nsPredicate = dynamicPredicate
+            }
             .navigationDestination(for: Pokemon.self) { pokemon in
                 Text(pokemon.name ?? "no name")
             }
