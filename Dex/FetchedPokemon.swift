e1730454e3c262aad030b7d1260661d4b5d2b145
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2025-07-30 16:34:55 -0300

    Download and store images with Core Data

--- Dex/FetchedPokemon.swift
+++ Dex/FetchedPokemon.swift
@@ -17,8 +17,8 @@ struct FetchedPokemon: Decodable {
     let specialAttack: Int16
     let specialDefense: Int16
     let speed: Int16
-    let sprite: URL
-    let shiny: URL
+    let spriteURL: URL
+    let shinyURL: URL
     
     enum CodingKeys: CodingKey {
         case id
@@ -40,9 +40,9 @@ struct FetchedPokemon: Decodable {
         }
         
         enum SpriteKeys: String, CodingKey {
-            case sprite = "frontDefault"
+            case spriteURL = "frontDefault"
             
-            case shiny = "frontShiny"
+            case shinyURL = "frontShiny"
         }
     }
     
@@ -90,7 +90,7 @@ struct FetchedPokemon: Decodable {
         speed = decodedStats[5]
         
         let spriteContainer = try container.nestedContainer(keyedBy: CodingKeys.SpriteKeys.self, forKey: .sprites)
-        sprite = try spriteContainer.decode(URL.self, forKey: .sprite)
-        shiny = try spriteContainer.decode(URL.self, forKey: .shiny)
+        spriteURL = try spriteContainer.decode(URL.self, forKey: .spriteURL)
+        shinyURL = try spriteContainer.decode(URL.self, forKey: .shinyURL)
     }
 }
