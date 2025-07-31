6c62f81f2ca21d1d5b1347f3ea35daa0c9d4bd5a
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2025-07-31 14:09:12 -0300

    Add widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Ana Clara Moreira Rodrigues on 31/07/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
