d2eb36aa8f4344709bf8d66ba79b18bd9b133cd3
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2025-08-12 15:29:39 -0300

    Update views to use SwiftData

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -6,17 +6,12 @@
 //
 
 import SwiftUI
-import CoreData
+import SwiftData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.modelContext) private var modelContext
     
-    @FetchRequest<Pokemon>(sortDescriptors: []) private var all
-
-    @FetchRequest<Pokemon>(
-        sortDescriptors: [SortDescriptor(\.id)],
-        animation: .default
-    ) private var pokedex
+    @Query(sort: \Pokemon.id, animation: .default) private var pokedex: [Pokemon]
     
     @State private var searchText = ""
     @State private var filterByFavorites = false
@@ -43,7 +38,7 @@ struct ContentView: View {
     }
 
     var body: some View {
-        if all.isEmpty {
+        if pokedex.isEmpty {
             ContentUnavailableView {
                 Label("No Pokemon", image: .nopokemon)
             } description: {
@@ -80,7 +75,7 @@ struct ContentView: View {
                                 
                                 VStack(alignment: .leading) {
                                     HStack {
-                                        Text("#\(pokemon.id) \(pokemon.name!.capitalized)")
+                                        Text("#\(pokemon.id) \(pokemon.name.capitalized)")
                                             .fontWeight(.bold)
                                         
                                         if pokemon.favorite {
@@ -90,7 +85,7 @@ struct ContentView: View {
                                     }
                                     
                                     HStack {
-                                        ForEach(pokemon.types!, id: \.self) { type in
+                                        ForEach(pokemon.types, id: \.self) { type in
                                             Text(type.capitalized)
                                                 .font(.subheadline)
                                                 .fontWeight(.semibold)
@@ -108,7 +103,7 @@ struct ContentView: View {
                                     pokemon.favorite.toggle()
                                     
                                     do {
-                                        try viewContext.save()
+                                        try modelContext.save()
                                     } catch {
                                         print(error)
                                     }
@@ -117,7 +112,7 @@ struct ContentView: View {
                             }
                         }
                     } footer: {
-                        if all.count < 151 {
+                        if pokedex.count < 151 {
                             ContentUnavailableView {
                                 Label("Missing Pokemon", image: .nopokemon)
                             } description: {
@@ -135,15 +130,8 @@ struct ContentView: View {
                 .navigationTitle("Pokedex")
                 .searchable(text: $searchText, prompt: "Find a Pokemon")
                 .autocorrectionDisabled()
-                .onChange(of: searchText) {
-                    pokedex.nsPredicate = dynamicPredicate
-                }
-                .onChange(of: filterByFavorites) {
-                    pokedex.nsPredicate = dynamicPredicate
-                }
                 .navigationDestination(for: Pokemon.self) { pokemon in
-                    PokemonDetail()
-                        .environmentObject(pokemon)
+                    PokemonDetail(pokemon: pokemon)
                 }
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
@@ -165,20 +153,7 @@ struct ContentView: View {
                 do {
                     let fetchedPokemon = try await fetcher.fetchPokemon(i)
                     
-                    let pokemon = Pokemon(context: viewContext)
-                    pokemon.id = fetchedPokemon.id
-                    pokemon.name = fetchedPokemon.name
-                    pokemon.types = fetchedPokemon.types
-                    pokemon.hp = fetchedPokemon.hp
-                    pokemon.attack = fetchedPokemon.attack
-                    pokemon.defense = fetchedPokemon.defense
-                    pokemon.specialAttack = fetchedPokemon.specialAttack
-                    pokemon.specialDefense = fetchedPokemon.specialDefense
-                    pokemon.speed = fetchedPokemon.speed
-                    pokemon.spriteURL = fetchedPokemon.spriteURL
-                    pokemon.shinyURL = fetchedPokemon.shinyURL
-                    
-                    try viewContext.save()
+                    modelContext.insert(fetchedPokemon)
                 } catch {
                     print(error)
                 }
@@ -191,12 +166,12 @@ struct ContentView: View {
     private func storeSprites() {
         Task {
             do {
-                for pokemon in all {
-                    pokemon.sprite = try await URLSession.shared.data(from: pokemon.spriteURL!).0
-                    pokemon.shiny = try await URLSession.shared.data(from: pokemon.shinyURL!).0
+                for pokemon in pokedex {
+                    pokemon.sprite = try await URLSession.shared.data(from: pokemon.spriteURL).0
+                    pokemon.shiny = try await URLSession.shared.data(from: pokemon.shinyURL).0
                     
-                    try viewContext.save()
-                    print("Sprites stored: \(pokemon.id):\(pokemon.name!.capitalized)")
+                    try modelContext.save()
+                    print("Sprites stored: \(pokemon.id):\(pokemon.name.capitalized)")
                 }
             } catch {
                 print(error)
@@ -206,5 +181,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
+        .modelContainer(PersistenceController.preview)
 }
--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -8,6 +8,7 @@
 import SwiftData
 import Foundation //JSONDecoder
 
+// When the stuf in here is going to type directly to the view
 @MainActor
 struct PersistenceController {
     static var previewPokemon: Pokemon {
