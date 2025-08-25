36527b3f68300c27de05e6b13c4d8791b306bcda
AnaMRodrigues <aninha_s2.acrm@icloud.com>
2025-08-25 17:04:19 -0300

    Update widget to use SwiftData

--- DexWidget/DexWidgetBundle.swift
+++ DexWidget/DexWidgetBundle.swift
@@ -1,16 +1,16 @@
-////
-////  DexWidgetBundle.swift
-////  DexWidget
-////
-////  Created by Ana Clara Moreira Rodrigues on 31/07/25.
-////
 //
-//import WidgetKit
-//import SwiftUI
+//  DexWidgetBundle.swift
+//  DexWidget
 //
-//@main
-//struct DexWidgetBundle: WidgetBundle {
-//    var body: some Widget {
-//        DexWidget()
-//    }
-//}
+//  Created by Ana Clara Moreira Rodrigues on 31/07/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
